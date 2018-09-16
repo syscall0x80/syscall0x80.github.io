6dc75dfa03feb5edd636298cc2a8469e2370ce18
任永强 <yongqiang@blacklake.cn>
2018-09-16 23:06:48 +0800

    Site updated: 2018-09-16 23:06:46

---
+++ static/gitment.js
@@ -0,0 +1,9 @@
+var gitment = new Gitment({
+  owner: 'yongqiangren',
+  repo: 'yongqiangren.github.io',
+  oauth: {
+    client_id: '04b0e93a41c5229f8661',
+    client_secret: '65c40e2d0afff760c0528f4d169e2e919f1a7123',
+  },
+});
+gitment.render('container');
